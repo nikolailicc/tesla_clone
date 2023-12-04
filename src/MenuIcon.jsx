eeffe5a8bf664c2dbf9fe335989459fce32d5999
nikolailicc <nidzo205@gmail.com>
2023-12-04 17:28:03 +0100

    initial

---
+++ src/MenuIcon.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./components/MenuIcon.css";
+
+function MenuIcon() {
+	return <i className="fa-solid fa-bars"></i>;
+}
+
+export default MenuIcon;
---
+++ src/components/MenuCloseIcon.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function MenuCloseIcon() {
+	return;
+
+	<i className="fa-solid fa-x"></i>;
+}
+
+export default MenuCloseIcon;
